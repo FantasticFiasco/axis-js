a7da5511ceef4076491087d7e14ad0f5d58c3827
FantasticFiasco <mattias.kindborg@gmail.com>
2020-11-01 09:09:29 +0100

    wip

--- samples/axis-configuration/sample.ts
+++ samples/axis-configuration/sample.ts
@@ -3,7 +3,7 @@ import * as parameters from './parameters';
 import * as users from './users';
 
 (async () => {
-    const connection = new Connection(Protocol.Http, '192.168.1.102', 80, 'admin', '32naJzkJdZ!7*HK&Dz');
+    const connection = new Connection(Protocol.Http, '<ip address>', 80, 'root', '<password>');
 
     await parameters.run(connection);
     await users.run(connection);
