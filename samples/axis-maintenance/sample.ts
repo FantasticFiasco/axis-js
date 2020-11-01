536679fe2fcc7ac2bfaf85ff3fb6e56856fbf1a4
FantasticFiasco <mattias.kindborg@gmail.com>
2020-11-01 09:56:32 +0100

    wip

---
+++ samples/axis-maintenance/sample.ts
@@ -0,0 +1,18 @@
+import { Connection, FactoryDefaultType, Maintenance, Protocol } from 'axis-maintenance';
+
+(async () => {
+    const connection = new Connection(Protocol.Http, '<ip address>', 80, 'root', '<password>');
+    const maintenance = new Maintenance(connection);
+
+    // Restart
+    await maintenance.restart();
+    console.log('Camera accepted request to restart');
+
+    // Partial factory default
+    await maintenance.factoryDefault(FactoryDefaultType.Partial);
+    console.log('Camera accepted request to factory default');
+
+    // Hard factory default
+    await maintenance.factoryDefault(FactoryDefaultType.Hard);
+    console.log('Camera accepted request to factory default');
+})();
