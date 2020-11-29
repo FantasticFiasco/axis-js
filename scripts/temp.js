f40bb1865e3ef6807483b998565974cd4868a620
FantasticFiasco <mattias.kindborg@gmail.com>
2020-11-30 00:35:05 +0100

    wip

--- scripts/temp.js
+++ scripts/temp.js
@@ -1,5 +1,5 @@
 const { REPO, GITHUB_TOKEN, NPM_TOKEN } = require('./github-actions');
 
-console.log('temp repo', REPO.length);
+//console.log('temp repo', REPO.length);
 console.log('temp gh token', GITHUB_TOKEN.length);
 console.log('temp npm', NPM_TOKEN.length);
