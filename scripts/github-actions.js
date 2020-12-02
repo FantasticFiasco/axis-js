bbd0d7a4bd870086d249c3becb2f0e597aaa25ed
FantasticFiasco <mattias.kindborg@gmail.com>
2020-12-02 21:30:36 +0100

    wip

--- scripts/github-actions.js
+++ scripts/github-actions.js
@@ -2,7 +2,7 @@
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const NPM_TOKEN = process.env.NPM_TOKEN;
-const GIT_TAG = process.env.TRAVIS_TAG;
+const GIT_TAG = process.env.GIT_TAG;
 const REPO = process.env.TRAVIS_REPO_SLUG;
 
 module.exports = {
