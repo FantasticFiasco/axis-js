c8dece77a07452fdd838dc79ac67bb6a7ef46f32
FantasticFiasco <mattias.kindborg@gmail.com>
2021-01-03 11:44:38 +0100

    wip

--- packages/axis-snapshot/src/requests/auth.ts
+++ packages/axis-snapshot/src/requests/auth.ts
@@ -1,15 +1,88 @@
-import got from 'got';
+import got, { HTTPError, Response } from 'got';
 
-export const basicAuth = async (url: string, username: string, password: string) => {
-    const auth = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+export const get = async (url: string, username: string, password: string) => {
+    // Send request without authentication
+    let { error, res } = await noAuth(url);
 
-    return await got(url, {
-        headers: {
-            Authorization: auth,
-        },
-    });
+    if (error === undefined) {
+        return res;
+    }
+
+    if (error.response.statusCode !== 401) {
+        throw error;
+    }
+
+    const wwwAuthenticate = error.response.headers['www-authenticate'];
+    if (wwwAuthenticate === undefined) {
+        throw error;
+    }
+
+    const protocol = wwwAuthenticate.split(' ')[0];
+    switch (protocol) {
+        case 'Basic':
+            ({ error, res } = await basicAuth(url, username, password));
+            if (error !== undefined) {
+                throw error;
+            }
+            return res;
+
+        case 'Digest':
+            console.log('digest');
+            break;
+
+        default:
+            throw error;
+    }
+
+    return res;
 };
 
-// export const digestAuth = (username: string, password: string) => {
-//     throw new Error('Not implemented.');
+const noAuth = async (url: string): Promise<{ error: HTTPError; res: undefined } | { error: undefined; res: Response<string> }> => {
+    try {
+        const res = await got.get(url);
+        return {
+            error: undefined,
+            res,
+        };
+    } catch (error) {
+        return {
+            error,
+            res: undefined,
+        };
+    }
+};
+
+const basicAuth = async (
+    url: string,
+    username: string,
+    password: string
+): Promise<{ error: HTTPError; res: undefined } | { error: undefined; res: Response<string> }> => {
+    const auth = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+
+    try {
+        const res = await got(url, {
+            headers: {
+                Authorization: auth,
+            },
+        });
+        return {
+            error: undefined,
+            res,
+        };
+    } catch (error) {
+        return {
+            error,
+            res: undefined,
+        };
+    }
+};
+
+// const digestAuth = async (url: string, username: string, password: string) => {
+//     const auth = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+
+//     return await got(url, {
+//         headers: {
+//             Authorization: auth,
+//         },
+//     });
 // };
--- packages/axis-snapshot/test/requests/auth.spec.ts
+++ packages/axis-snapshot/test/requests/auth.spec.ts
@@ -1,39 +1,95 @@
-import { HTTPError } from 'got';
-import { basicAuth } from '../../src/requests/auth';
+import { get } from '../../src/requests/auth';
 
 const USERNAME = 'root';
 const PASSWORD = 'pass';
-const BASIC_AUTH_URL = `http://httpbin.org/basic-auth/${USERNAME}/${PASSWORD}`;
 
-describe('#basicAuth should', () => {
-    test('successfully authenticate given valid credentials', async () => {
+const NO_AUTH_URL = 'https://www.google.com';
+const BASIC_AUTH_URL = `http://httpbin.org/basic-auth/${USERNAME}/${PASSWORD}`;
+// const DIGEST_AUTH_URL = `http://httpbin.org/digest-auth/auth/${USERNAME}/${PASSWORD}`;
+
+describe('#get should', () => {
+    test('succeed given no authentication', async () => {
         // Act
-        const res = await basicAuth(BASIC_AUTH_URL, USERNAME, PASSWORD);
+        const res = await get(NO_AUTH_URL, '', '');
 
         // Assert
-        expect(JSON.parse(res.body)).toStrictEqual({
+        expect(res?.body).toBeDefined();
+    });
+
+    test('succeed given basic authentication', async () => {
+        // Act
+        const res = await get(BASIC_AUTH_URL, USERNAME, PASSWORD);
+
+        // Assert
+        expect(JSON.parse(res!.body)).toStrictEqual({
             authenticated: true,
             user: USERNAME,
         });
     });
-
-    test('throw error given invalid credentials', async () => {
-        // Arrange
-        const testCases: { username: string; password: string }[] = [
-            { username: USERNAME, password: 'invalid password' },
-            { username: 'invalid username', password: PASSWORD },
-        ];
-
-        for (const { username, password } of testCases) {
-            // Act
-            try {
-                await basicAuth(BASIC_AUTH_URL, username, password);
-                throw new Error('This exception should not be thrown');
-            } catch (error) {
-                // Assert
-                expect(error).toBeInstanceOf(HTTPError);
-                expect((error as HTTPError).response.statusCode).toBe(401);
-            }
-        }
-    });
 });
+
+// describe('#basicAuth should', () => {
+//     test('successfully authenticate given valid credentials', async () => {
+//         // Act
+//         const res = await basicAuth(BASIC_AUTH_URL, USERNAME, PASSWORD);
+
+//         // Assert
+//         expect(JSON.parse(res.body)).toStrictEqual({
+//             authenticated: true,
+//             user: USERNAME,
+//         });
+//     });
+
+//     test('throw error given invalid credentials', async () => {
+//         // Arrange
+//         const testCases: { username: string; password: string }[] = [
+//             { username: USERNAME, password: 'invalid password' },
+//             { username: 'invalid username', password: PASSWORD },
+//         ];
+
+//         for (const { username, password } of testCases) {
+//             // Act
+//             try {
+//                 await basicAuth(BASIC_AUTH_URL, username, password);
+//                 throw new Error('This exception should not be thrown');
+//             } catch (error) {
+//                 // Assert
+//                 expect(error).toBeInstanceOf(HTTPError);
+//                 expect((error as HTTPError).response.statusCode).toBe(401);
+//             }
+//         }
+//     });
+// });
+
+// describe('#digestAuth should', () => {
+//     test('successfully authenticate given valid credentials', async () => {
+//         // Act
+//         const res = await digestAuth(DIGEST_AUTH_URL, USERNAME, PASSWORD);
+
+//         // Assert
+//         expect(JSON.parse(res.body)).toStrictEqual({
+//             authenticated: true,
+//             user: USERNAME,
+//         });
+//     });
+
+//     test('throw error given invalid credentials', async () => {
+//         // Arrange
+//         const testCases: { username: string; password: string }[] = [
+//             { username: USERNAME, password: 'invalid password' },
+//             { username: 'invalid username', password: PASSWORD },
+//         ];
+
+//         for (const { username, password } of testCases) {
+//             // Act
+//             try {
+//                 await digestAuth(DIGEST_AUTH_URL, username, password);
+//                 throw new Error('This exception should not be thrown');
+//             } catch (error) {
+//                 // Assert
+//                 expect(error).toBeInstanceOf(HTTPError);
+//                 expect((error as HTTPError).response.statusCode).toBe(401);
+//             }
+//         }
+//     });
+// });
