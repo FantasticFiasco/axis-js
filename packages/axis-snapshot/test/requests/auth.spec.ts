4d7d6df0d217e61fd4391d6f7e4ac2c1080a3766
FantasticFiasco <mattias.kindborg@gmail.com>
2021-01-03 11:55:45 +0100

    wip

--- packages/axis-snapshot/test/requests/auth.spec.ts
+++ packages/axis-snapshot/test/requests/auth.spec.ts
@@ -1,3 +1,4 @@
+import { HTTPError } from 'got';
 import { get } from '../../src/requests/auth';
 
 const USERNAME = 'root';
@@ -5,7 +6,7 @@ const PASSWORD = 'pass';
 
 const NO_AUTH_URL = 'https://www.google.com';
 const BASIC_AUTH_URL = `http://httpbin.org/basic-auth/${USERNAME}/${PASSWORD}`;
-// const DIGEST_AUTH_URL = `http://httpbin.org/digest-auth/auth/${USERNAME}/${PASSWORD}`;
+const DIGEST_AUTH_URL = `http://httpbin.org/digest-auth/auth/${USERNAME}/${PASSWORD}`;
 
 describe('#get should', () => {
     test('succeed given no authentication', async () => {
@@ -26,6 +27,41 @@ describe('#get should', () => {
             user: USERNAME,
         });
     });
+
+    test('succeed given digest authentication', async () => {
+        // Act
+        const res = await get(DIGEST_AUTH_URL, USERNAME, PASSWORD);
+
+        // Assert
+        expect(JSON.parse(res!.body)).toStrictEqual({
+            authenticated: true,
+            user: USERNAME,
+        });
+    });
+
+    test('throw error given invalid credentials', async () => {
+        // Arrange
+        const testCases: { url: string; username: string; password: string }[] = [
+            // Basic auth
+            { url: BASIC_AUTH_URL, username: USERNAME, password: 'invalid password' },
+            { url: BASIC_AUTH_URL, username: 'invalid username', password: PASSWORD },
+            // Digest auth
+            { url: DIGEST_AUTH_URL, username: USERNAME, password: 'invalid password' },
+            { url: DIGEST_AUTH_URL, username: 'invalid username', password: PASSWORD },
+        ];
+
+        for (const { username, password } of testCases) {
+            // Act
+            try {
+                await get(BASIC_AUTH_URL, username, password);
+                throw new Error('This exception should not be thrown');
+            } catch (error) {
+                // Assert
+                expect(error).toBeInstanceOf(HTTPError);
+                expect((error as HTTPError).response.statusCode).toBe(401);
+            }
+        }
+    });
 });
 
 // describe('#basicAuth should', () => {
@@ -40,25 +76,6 @@ describe('#get should', () => {
 //         });
 //     });
 
-//     test('throw error given invalid credentials', async () => {
-//         // Arrange
-//         const testCases: { username: string; password: string }[] = [
-//             { username: USERNAME, password: 'invalid password' },
-//             { username: 'invalid username', password: PASSWORD },
-//         ];
-
-//         for (const { username, password } of testCases) {
-//             // Act
-//             try {
-//                 await basicAuth(BASIC_AUTH_URL, username, password);
-//                 throw new Error('This exception should not be thrown');
-//             } catch (error) {
-//                 // Assert
-//                 expect(error).toBeInstanceOf(HTTPError);
-//                 expect((error as HTTPError).response.statusCode).toBe(401);
-//             }
-//         }
-//     });
 // });
 
 // describe('#digestAuth should', () => {
