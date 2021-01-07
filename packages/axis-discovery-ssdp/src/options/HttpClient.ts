bebe9dfaac6844ebcbc8deeb381fd57415a9d4b6
FantasticFiasco <mattias.kindborg@gmail.com>
2021-01-07 17:16:19 +0100

    wip

--- packages/axis-discovery-ssdp/src/options/HttpClient.ts
+++ packages/axis-discovery-ssdp/src/options/HttpClient.ts
@@ -1,19 +1,12 @@
 import * as expect from '@fantasticfiasco/expect';
-import { get } from 'request';
+import got from 'got';
 import { IHttpClient } from './IHttpClient';
 
 export class HttpClient implements IHttpClient {
-    public get(url: string): Promise<string> {
+    public async get(url: string): Promise<string> {
         expect.toExist(url);
 
-        return new Promise<string>((resolve, reject) => {
-            get(url, undefined, (error: any, _, body: string) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(body);
-                }
-            });
-        });
+        const res = await got.get(url);
+        return res.body;
     }
 }
