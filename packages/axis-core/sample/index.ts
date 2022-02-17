6f101390e7b383cd7490604b0db1b60c3fc44c17
FantasticFiasco <mattias@kindb.org>
2022-02-17 08:45:31 +0100

    wip

--- packages/axis-core/sample/index.ts
+++ packages/axis-core/sample/index.ts
@@ -4,7 +4,6 @@ import { Connection, get, Protocol, Response } from '../src';
     const connection = new Connection(Protocol.Http, '<ip address>', 80, 'root', '<password>');
     const res: Response = await get(connection, '/axis-cgi/param.cgi?action=list&group=Brand.ProdShortName');
 
-    //temp
     console.log('Status code:', res.statusCode);
     console.log('Headers:', res.headers);
     console.log('Body:', res.body);
