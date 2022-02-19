90528b59101b408cdb92e30406d67d20cd903dc1
FantasticFiasco <mattias@kindb.org>
2022-02-19 09:02:00 +0100

    wip

--- packages/axis-core/test/client.spec.ts
+++ packages/axis-core/test/client.spec.ts
@@ -35,8 +35,8 @@ describe('#get should', () => {
         const got = await get(connection, relativePath);
 
         // Assert
-        expect(got?.statusCode).toBe(200);
-        expect(got?.body.toString()).toBe('Success');
+        expect(got.statusCode).toBe(200);
+        expect(got.body.toString()).toBe('Success');
     });
 
     test('succeed given digest authentication', async () => {
@@ -47,8 +47,8 @@ describe('#get should', () => {
         const got = await get(connection, relativePath);
 
         // Assert
-        expect(got?.statusCode).toBe(200);
-        expect(got?.body.toString()).toBe('Success');
+        expect(got.statusCode).toBe(200);
+        expect(got.body.toString()).toBe('Success');
     });
 
     test('throw error given invalid credentials', async () => {
