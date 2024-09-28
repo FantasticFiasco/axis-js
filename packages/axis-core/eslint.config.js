7eb8b3fb00a16470bea4127119cdedcab2ac845c
FantasticFiasco <mattias@kindb.org>
2024-09-28 08:36:48 +0200

    wip

---
+++ packages/axis-core/eslint.config.js
@@ -0,0 +1,13 @@
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+
+module.exports = tseslint.config(
+    {
+        files: ["**/*.ts"],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.stylistic,
+        ],
+    },
+);
