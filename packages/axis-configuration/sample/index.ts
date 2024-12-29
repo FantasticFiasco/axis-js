19767619fd5d498a3da92c68ecad6668f65abf2c
FantasticFiasco <mattias@kindb.org>
2024-12-29 12:31:00 +0100

    wip

--- packages/axis-configuration/sample/index.ts
+++ packages/axis-configuration/sample/index.ts
@@ -2,8 +2,12 @@ import { Connection, Protocol } from 'axis-core';
 import * as parameters from './parameters';
 import * as users from './users';
 
+const address = process.env.DEVICE_IP ?? '192.168.0.90';
+const username = process.env.DEVICE_USERNAME ?? 'root';
+const password = process.env.DEVICE_PASSWORD ?? 'pass';
+
 (async () => {
-    const connection = new Connection(Protocol.Http, '<ip address>', 80, 'root', '<password>');
+    const connection = new Connection(Protocol.Http, address, 80, username, password);
 
     await parameters.run(connection);
     await users.run(connection);
