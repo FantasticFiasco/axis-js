c957cdf64bb6933e5d1095717d5e87c576cdefbb
FantasticFiasco <mattias@kindb.org>
2024-12-29 12:13:15 +0100

    wip

--- packages/axis-core/sample/index.ts
+++ packages/axis-core/sample/index.ts
@@ -1,10 +1,16 @@
 import { Connection, get, Protocol } from '../src';
 
+const address = process.env.DEVICE_IP ?? '192.168.0.90';
+const username = process.env.DEVICE_USERNAME ?? 'root';
+const password = process.env.DEVICE_PASSWORD ?? 'pass';
+
 (async () => {
-    const connection = new Connection(Protocol.Http, '<ip address>', 80, 'root', '<password>');
+    const connection = new Connection(Protocol.Http, address, 80, username, password);
+
     const res: Response = await get(connection, '/axis-cgi/param.cgi?action=list&group=Brand.ProdShortName');
+    const body = await res.text();
 
     console.log('Status:', res.status);
     console.log('Headers:', res.headers);
-    console.log('Body:', await res.text());
+    console.log('Body:', body);
 })();
