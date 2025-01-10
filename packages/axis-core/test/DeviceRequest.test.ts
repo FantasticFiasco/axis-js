73496ac605620855d841aca7d96afe9eec311092
FantasticFiasco <mattias@kindb.org>
2025-01-10 15:30:58 +0100

    wip

--- packages/axis-core/test/DeviceRequest.test.ts
+++ packages/axis-core/test/DeviceRequest.test.ts
@@ -1,108 +1,54 @@
-// import { URL } from 'url';
-// import { Connection } from '../src/Connection';
-// import { DeviceRequest } from '../src/DeviceRequest';
-// import { Protocol } from '../src/Protocol';
-// import { WebServer } from './WebServer';
+import { Connection, Protocol } from '../src';
+import { DeviceRequest } from '../src/DeviceRequest';
 
-test('placeholder', () => {
-    expect(true).toBe(true);
+class TestDeviceRequest extends DeviceRequest {
+    constructor(connection: Connection, relativePath: string) {
+        super(connection, relativePath);
+    }
+}
+
+describe('should', () => {
+    test('return http url', () => {
+        // Arrange
+        const connection = new Connection(Protocol.Http, '1.2.3.4', 100, 'root', 'pass');
+
+        // Act
+        const got = new TestDeviceRequest(connection, '/test');
+
+        // Assert
+        expect(got.url).toBe('http://1.2.3.4:100/test');
+    });
+
+    test('return https url', () => {
+        // Arrange
+        const connection = new Connection(Protocol.Https, '1.2.3.4', 100, 'root', 'pass');
+
+        // Act
+        const got = new TestDeviceRequest(connection, '/test');
+
+        // Assert
+        expect(got.url).toBe('https://1.2.3.4:100/test');
+    });
+
+    test('return username', () => {
+        // Arrange
+        const connection = new Connection(Protocol.Https, '1.2.3.4', 80, 'root', 'pass');
+
+        // Act
+        const got = new TestDeviceRequest(connection, '/test');
+
+        // Assert
+        expect(got.username).toBe('root');
+    });
+
+    test('return password', () => {
+        // Arrange
+        const connection = new Connection(Protocol.Https, '1.2.3.4', 80, 'root', 'pass');
+
+        // Act
+        const got = new TestDeviceRequest(connection, '/test');
+
+        // Assert
+        expect(got.password).toBe('pass');
+    });
 });
-
-// let webServer: WebServer;
-
-// class TestRequest extends DeviceRequest {
-//     constructor(connection: Connection) {
-//         super(connection);
-//     }
-
-//     public async send(relativePath: string): Promise<Response> {
-//         return this._get(relativePath);
-//     }
-// }
-
-// beforeAll(async () => {
-//     webServer = new WebServer();
-//     await webServer.listen('127.0.0.1', 0);
-// });
-
-// afterAll(async () => {
-//     await webServer.close();
-// });
-
-// describe('should', () => {
-//     test('succeed given no authentication', async () => {
-//         // Arrange
-//         const { connection, relativePath } = parseUrl(webServer.guestUri, '', '');
-//         const req = new TestRequest(connection);
-
-//         // Act
-//         const got = await req.send(relativePath);
-
-//         // Assert
-//         expect(got.status).toBe(200);
-//         await expect(got.text()).resolves.toBe('Success');
-//     });
-
-//     test('succeed given basic authentication', async () => {
-//         // Arrange
-//         const { connection, relativePath } = parseUrl(webServer.guestUri, webServer.username, webServer.password);
-//         const req = new TestRequest(connection);
-
-//         // Act
-//         const got = await req.send(relativePath);
-
-//         // Assert
-//         expect(got.status).toBe(200);
-//         await expect(got.text()).resolves.toBe('Success');
-//     });
-
-//     test('succeed given digest authentication', async () => {
-//         // Arrange
-//         const { connection, relativePath } = parseUrl(webServer.digestAuthUri, webServer.username, webServer.password);
-//         const req = new TestRequest(connection);
-
-//         // Act
-//         const got = await req.send(relativePath);
-
-//         // Assert
-//         expect(got.status).toBe(200);
-//         await expect(got.text()).resolves.toBe('Success');
-//     });
-
-//     test('return status 401 given invalid credentials', async () => {
-//         // Arrange
-//         const testCases: { url: string; username: string; password: string }[] = [
-//             // Basic auth
-//             { url: webServer.basicAuthUri, username: webServer.username, password: 'invalid password' },
-//             { url: webServer.basicAuthUri, username: 'invalid username', password: webServer.password },
-//             // Digest auth
-//             { url: webServer.digestAuthUri, username: webServer.username, password: 'invalid password' },
-//             { url: webServer.digestAuthUri, username: 'invalid-username', password: webServer.password },
-//         ];
-
-//         for (const { url, username, password } of testCases) {
-//             const { connection, relativePath } = parseUrl(url, username, password);
-//             const req = new TestRequest(connection);
-
-//             // Act
-//             const got = await req.send(relativePath);
-//             expect(got.status).toBe(401);
-//         }
-//     });
-// });
-
-// const parseUrl = (url: string, username: string, password: string): { connection: Connection; relativePath: string } => {
-//     const { protocol, hostname, port, pathname } = new URL(url);
-
-//     if (protocol !== 'http:') {
-//         throw new Error('Tests are currently not written to support anything else than HTTP');
-//     }
-
-//     const connection = new Connection(Protocol.Http, hostname, Number.parseInt(port), username, password);
-//     const relativePath = pathname;
-
-//     return {
-//         connection,
-//         relativePath,
-//     };
-// };
