631b8d144035b63b9752e36eee353b9f8b314837
FantasticFiasco <mattias@kindb.org>
2025-01-20 08:15:04 +0100

    wip

--- packages/axis-core/sample/index.ts
+++ packages/axis-core/sample/index.ts
@@ -5,14 +5,20 @@ const port = Number.parseInt(process.env.DEVICE_PORT ?? '80');
 const username = process.env.DEVICE_USERNAME ?? 'root';
 const password = process.env.DEVICE_PASSWORD ?? 'pass';
 
+// fetchBuilder is building a version of fetch that can handle the
+// authentication protocols used by Axis devices.
 const fetch = fetchBuilder(global.fetch);
 
+// This package is exporting utility classes and methods. In this case we rely
+// on DeviceRequest to build a request that is reading the product short name.
 class GetProdShortNameRequest extends DeviceRequest {
     constructor(connection: Connection) {
         super(connection, '/axis-cgi/param.cgi?action=list&group=Brand.ProdShortName');
     }
 }
 
+// This is a custom response handler that reads the response body and extracts
+// the parameter name and value from the text.
 const responseHandler = async (res: Response): Promise<{ name: string; value: string }> => {
     if (!res.ok) {
         throw new Error(`Request failed with status ${res.status} ${res.statusText}`);
