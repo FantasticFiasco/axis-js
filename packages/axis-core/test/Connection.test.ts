46f8ae8c0bf7741fae65f9906a4890f66a8ed399
FantasticFiasco <mattias@kindb.org>
2025-01-20 08:19:39 +0100

    wip

--- packages/axis-core/test/Connection.test.ts
+++ packages/axis-core/test/Connection.test.ts
@@ -5,20 +5,20 @@ describe('connection', () => {
     describe('#url', () => {
         test('should return http url', () => {
             // Act
-            const got = new Connection(Protocol.Http, '1.2.3.4', 80, 'root', 'pass');
+            const got = new Connection(Protocol.Http, '1.2.3.4', 1234, 'root', 'pass');
 
             // Assert
             expect(got).toBeTruthy();
-            expect(got.url).toBe('http://1.2.3.4:80');
+            expect(got.url).toBe('http://1.2.3.4:1234');
         });
 
         test('should return https url', () => {
             // Act
-            const got = new Connection(Protocol.Https, '1.2.3.4', 80, 'root', 'pass');
+            const got = new Connection(Protocol.Https, '1.2.3.4', 1234, 'root', 'pass');
 
             // Assert
             expect(got).toBeTruthy();
-            expect(got.url).toBe('https://1.2.3.4:80');
+            expect(got.url).toBe('https://1.2.3.4:1234');
         });
     });
 });
