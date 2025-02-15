ddaffa73c971d436a343fdd5bf3f1413c920033c
FantasticFiasco <mattias@kindb.org>
2025-02-15 09:35:30 +0100

    wip

--- packages/axis-discovery-ssdp/src/root-descriptions/RootDescriptionRequest.ts
+++ packages/axis-discovery-ssdp/src/root-descriptions/RootDescriptionRequest.ts
@@ -3,12 +3,18 @@ import { IHttpClient } from '../options';
 import { RootDescription } from './RootDescription';
 
 export class RootDescriptionRequest {
-    constructor(private readonly remoteAddress: string, private readonly location: string, private readonly httpClient: IHttpClient) {}
+    constructor(
+        private readonly remoteAddress: string,
+        private readonly location: string,
+        private readonly httpClient: IHttpClient,
+    ) {}
 
     public async send(): Promise<RootDescription> {
         log('RootDescriptionRequest#send - %s', this.remoteAddress);
 
-        const body = await this.httpClient.get(this.location);
-        return RootDescription.parse(this.remoteAddress, body);
+        const res = await this.httpClient.get(this.location);
+        const text = await res.text();
+
+        return RootDescription.parse(this.remoteAddress, text);
     }
 }
